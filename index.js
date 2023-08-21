60844409ff68431f6d4c0c9b5d47ab7bc6aaf819
Karla Lockhart <kblockhart@gmail.com>
2023-08-21 11:03:10 -0700

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+// moveDodgerLeft()
+
+// moveDodgerRight()
+
+
+//const dodger = document.getElementById("dodger");
+
+// dodger.style.backgroundColor = "#FF69B4";
+
+// dodger.style.bottom = "100px";
+
+//dodger.style.left = "0px";
+
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerRight();
+    }
+  });
